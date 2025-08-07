db5e7b9e51200dce9bdf06d976ab715b82306ba3
changsin <changsin@strac.io>
2025-08-07 17:37:06 +0900

    cleanup_notebook script to fix metadata widget issues with Jupyter notebooks

---
+++ cleanup_notebook.py
@@ -0,0 +1,15 @@
+import nbformat
+
+file_path = "./notebooks/promt_token_counts.ipynb"
+
+with open(file_path, "r", encoding="utf-8") as f:
+    nb = nbformat.read(f, as_version=4)
+
+if 'widgets' in nb['metadata']:
+    print("Found corrupted widgets metadata. Removing it.")
+    del nb['metadata']['widgets']
+
+with open(file_path, "w", encoding="utf-8") as f:
+    nbformat.write(nb, f)
+
+print("Notebook cleaned.")
